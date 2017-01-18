2b326440109c92f2923fd5d8b8cf1fdd7e166d8a
Denis Cherednikov <denis.cherednikov@gmail.com>
2017-01-18 15:45:56 +0300

    delete extra indent

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -5,8 +5,8 @@ def get_roots(a, b, c):
     if discriminant < 0:
     	return None, None
     else:
-	root1 = (-b - sqrt(discriminant)) / (2 * a)
-	root2 = (-b + sqrt(discriminant)) / (2 * a)
+        root1 = (-b - sqrt(discriminant)) / (2 * a)
+        root2 = (-b + sqrt(discriminant)) / (2 * a)
     if discriminant == 0:
         return root1, None
     else:
